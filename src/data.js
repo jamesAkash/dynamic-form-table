e5240ab591348a53611d5b9d9448a02095537731
jamesAkash <akash.james@outlook.com>
2023-05-26 17:59:12 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,18 @@
+const data = [
+  {
+    id: 1,
+    name: "Bertie Yates",
+    mobile: "02/12/1984",
+    address: "jsbcsj sjbcks shbjcs",
+    email: "sahb@jhjb.com",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    mobile: "18/09/1997",
+    address: "jsbcsj sjbcks shbjcs",
+    email: "sahb@jhjb.com",
+  },
+];
+
+export default data;
