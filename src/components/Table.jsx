9320329738e86a9062d44c0e4eba3598720c8bce
jamesAkash <akash.james@outlook.com>
2023-05-26 18:07:34 +0530

    addition of mui

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
 const Table = () => {
-  return <div>Table</div>;
+  return (
+    <Wrapper>
+      <table>
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>React</td>
+            <td>Finish useContext next</td>
+            <td>
+              <span>Live</span>
+            </td>
+            <td>
+              <span></span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div``;
 
 export default Table;
